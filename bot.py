df70713969774d4a07e7e1890f98e587f4d52f34
cajoshuapark <41726552+cajoshuapark@users.noreply.github.com>
2022-03-20 02:35:40 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,24 @@
-from operator import truediv
+
 from config import API_KEY, API_SECRET , LOGIN, PASSWORD
+from binance.client import Client
+from operator import truediv
+import matplotlib.pyplot as plt 
+import mplfinance as mpf
+import pandas as pd
+import schedule
 import ccxt
 import json
-import schedule
-import time
-import numpy as np
-import pandas as pd
-from binance.client import Client
-import mplfinance as mpf
-import matplotlib.pyplot as plt
-
-ndaxCryptoName = 'MANA/CAD'
-cryptoName = 'MANA'
-
-#if true, then you can buy, if false, you can sell
+ 
 tradeBoolean = True
 
 def fetchData():
+    #if true, then you can buy, if false, you can sell
+    exchangeCryptoName = 'MANA/CAD'
+    cryptoName = 'MANA'
+    binanceCryptoName = 'BTCUSDT'
+
     #accessing ndax account through ccxt library 
-    ndax = ccxt.ndax({
+    exchange = ccxt.ndax({
         'apiKey': API_KEY,
         'secret': API_SECRET,
         'uid': "237456",   
@@ -28,8 +28,8 @@ def fetchData():
     })
 
     #fetching balance
-    balance = ndax.fetch_balance()
-    ticker = ndax.fetch_ticker(ndaxCryptoName)
+    balance = exchange.fetch_balance()
+    ticker = exchange.fetch_ticker(exchangeCryptoName)
     #formatting json response
     json.dumps(balance, sort_keys=True, indent=4)
     json.dumps(ticker, sort_keys=True, indent=4)
@@ -43,26 +43,28 @@ def fetchData():
     #accessing binance client using binance library 
     client = Client()
     #fetching historical OHLCV values using binance client
-    historical = client.get_historical_klines('BTCUSDT', client.KLINE_INTERVAL_1DAY, '1-Oct-2021')
-    
+    historical = client.get_historical_klines(
+        binanceCryptoName, client.KLINE_INTERVAL_1DAY, '1-Jan-2019'
+    )
+
     df = pd.DataFrame(historical)
     
     # data cleaning
     df.columns = ['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote Asset Volume', 'Number of Trades', 'TB Base Volume', 'TB Quote Volume', 'Ignore']
+    dateTimeColumns = ['Open Time','Close Time']
     df['Open Time'] = pd.to_datetime(df['Open Time']/1000, unit='s')
     df['Close Time'] = pd.to_datetime(df['Close Time']/1000, unit='s')
     floatColumns = ['Open', 'High', 'Low','Close', 'Volume','Quote Asset Volume', 'TB Base Volume', 'TB Quote Volume']
     df[floatColumns]= df[floatColumns].astype('float64')
     df = df.set_index('Close Time')
     
-    #plot candlestick graph with moving averages
-    mpf.plot(df.tail(60),
+   #plot candlestick graph with moving averages
+    mpf.plot(df.tail(100),
             type='candle',style='yahoo',volume= True,
-            title='BTCUSDT--- LAST 60 days',
+            title= binanceCryptoName + ' --- LAST 100 days',
             mav=(10,15))
 
     #data cleaning
-    df.tail(100)["Close"].mean()
     df['mav10'] = df['Close'].rolling(10).mean()
     df['mav15'] = df['Close'].rolling(15).mean()
     df = df.dropna()
@@ -90,17 +92,19 @@ def fetchData():
     plt.legend()
     print(plt.show())
 
+    #checking trades for today
     if df.mav10.iloc[len(df)-1] > df.mav15.iloc[len(df)-1] \
     and df.mav10.iloc[len(df)-2] < df.mav15.iloc[len(df)-2] \
     and tradeBoolean == True:
         print('buy today')  
-        #print(ndax.id, ndax.create_market_buy_order(ndaxCryptoName, purchaseAmount))  
+        print(exchange.id, exchange.create_market_buy_order(exchangeCryptoName, purchaseAmount))  
     if df.mav10.iloc[len(df)-1] < df.mav15.iloc[len(df)-1] \
     and df.mav10.iloc[len(df)-2] > df.mav15.iloc[len(df)-2] \
     and tradeBoolean == False:
         print('sell today')  
-        #print(ndax.id, ndax.create_market_sell_order(ndaxCryptoName, availableCrypto))
+        print(exchange.id, exchange.create_market_sell_order(exchangeCryptoName, availableCrypto))
 
+#call fetchData depending on time below
 schedule.every(12).hours.do(fetchData)
 
 #continuous data fetching loop
